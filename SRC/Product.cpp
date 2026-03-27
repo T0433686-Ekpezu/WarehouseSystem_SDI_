1f373a5fbabb672fa0de91262abe8c4051e14be1
T0433686-Ekpezu <T0433686@my.ntu.ac.uk>
2026-03-27 00:35:20 +0000

    ....

--- SRC/Product.cpp
+++ SRC/Product.cpp
@@ -1,3 +1,35 @@
-//
-// Created by ekpez on 26/03/2026.
-//
+#include "Product.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//Constructor(initialises all attributes)
+Product::Product(string id, string name, int qty, string loc)
+    : productID(id), name(name), quantity(qty), location(loc) {}
+
+//Getters(return private data safely)
+string Product::getProductID() const { return productID; }
+string Product::getName() const { return name; }
+int Product::getQuantity() const { return quantity; }
+string Product::getLocation() const { return location; }
+
+//Setters(allow controlled modification)
+void Product::setName(string name) { this->name = name; }
+
+void Product::setQuantity(int qty) {
+    if (qty >= 0) {
+        this->quantity = qty;
+    } else {
+        cout << "Error: Quantity cannot be negative." << endl;
+    }
+}
+
+void Product::setLocation(string loc) { this->location = loc; }
+
+//Display formatted product information
+void Product::display() const {
+    cout << left << setw(12) << productID
+         << setw(25) << name
+         << setw(10) << quantity
+         << setw(15) << location << endl;
+}
--- SRC/Product.h
+++ SRC/Product.h
@@ -2,3 +2,36 @@
 #define PRODUCT_H
 
 #include <string>
+using namespace std;
+
+
+//Product class(represents a single item in the warehouse)
+//It demonstrates encapsulation(private data,public getters/setters)
+class Product {
+    private:
+    string productID;
+    string name;
+    int quantity;
+    string location;
+
+    public:
+    //constructor
+    Product(string id, string name, int qty, string loc);
+
+    //Getter
+    string getProductID() const;
+    string getName() const;
+    string getQuantity() const;
+    string getLocation() const;
+
+    //Setter
+    void setName(string name);
+    void setQuantity(int qty);
+    void setLocation(string loc);
+
+    //Display product info
+    void display() const;
+
+};
+
+#endif
